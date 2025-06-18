31cbce5bfc0445b463cc853ac5efefa4e669495a
minhtu270404 <minhtu270404>
2025-06-18 13:48:07 +0700

    a

--- public/index.php
+++ public/index.php
@@ -30,8 +30,9 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 | into the script here so we don't need to manually load our classes.
 |
 */
+require __DIR__.'/vendor/autoload.php';
 
-require __DIR__.'/../vendor/autoload.php';
+
 
 /*
 |--------------------------------------------------------------------------
@@ -44,7 +45,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
